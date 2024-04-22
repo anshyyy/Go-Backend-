9d481dedc354e7c1a46e0d0e6214230fadaf0ed4
Anshuman Sharma <anshuman9998@gmail.com>
2024-04-22 21:52:26 +0530

    go routing

--- server/main.go
+++ server/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"example.com/todoServer/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
 	routes.InitalizeRoutes(r)
+	if err := godotenv.Load(); err != nil {
+		log.Panic("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	func() {
-		if err := r.Run(":3000"); err != nil {
+		if err := r.Run(":" + port); err != nil {
 			log.Fatal("Error occured starting the server", err)
 		}
 	}()
